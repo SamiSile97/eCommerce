dfe5f53f9c1cbb9c31cd110f1c1e065e9eec35c2
samisile <samisile97@gmail.com>
2023-10-06 17:13:41 +0100

    added header and an item card

---
+++ e-commerce/src/components/Header/header.js
@@ -0,0 +1,10 @@
+import "./header.css";
+function Header() {
+  return (
+    <div className="header">
+      <h1 className="header-title">Fake Store API APP</h1>
+    </div>
+  );
+}
+
+export default Header;
---
+++ e-commerce/src/components/ItemCard/itemCard.js
@@ -0,0 +1,21 @@
+import "./ItemCard.css";
+
+function ItemCard({ item }) {
+
+
+  let itemCard = item.map(({ image, title, price, id }) => {
+    return (
+      <div className="itemCard" key={id}>
+        <div className="imgDiv">
+          <img src={image} alt="rucksack"></img>
+        </div>
+        <p>{title}</p>
+        <p>£{price}</p>
+      </div>
+    );
+  });
+
+  return <div className="itemList">{itemCard}</div>;
+}
+
+export default ItemCard;
